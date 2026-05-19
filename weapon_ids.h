160b67bfb3118ae6ea27d9dd2e7dc5e48b87f595
birthday_attack <22209243+birthdayattack@users.noreply.github.com>
2026-05-19 17:28:59 -0500

    Initial map version of item ids, sorted into normal, unupgradeable, unique upgrade

---
+++ weapon_ids.h
@@ -0,0 +1,11 @@
+#ifndef WEAPON_IDS_H
+
+#define WEAPON_IDS_H
+
+#include <map>
+#include <string>
+extern std::map<uint64_t, std::string> weapon_ids;
+extern std::map<uint64_t, std::string> unupgradeable_weapons;
+extern std::map<uint64_t, std::string> unique_weapons;
+
+#endif
